44afd9b5e1cc1ac685de974c9f7f52b62381edb6
Elia Renzoni <elia.renzoni03@gmail.com>
2026-07-22 08:36:59 +0200

    fix
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- src/server/http_server.hpp
+++ src/server/http_server.hpp
@@ -6,8 +6,8 @@ namespace server {
  
 class Http : public stream::NetworkStream {
     public:
-        Http();
-        Http(std::string& addr, int& port, int& maxHeaderBytes);
+        Http(std::string& addr, int port): address(addr), port(port), maxHeaderBytes(16000), stream::NetworkStream(addr, port) {};
+        Http(std::string addr, int port, int maxHeaderBytes): address(addr), port(port), maxHeaderBytes(maxHeaderBytes), stream::NetworkStream(addr, port){};
         ~Http() = default;
 
         void listenAndServe();
