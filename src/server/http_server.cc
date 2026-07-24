584b277a66d8cda8fff8b77d05bc4acb8cd5142c
Elia Renzoni <elia.renzoni03@gmail.com>
2026-07-24 17:51:34 +0200

    fix
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- src/server/http_server.cc
+++ src/server/http_server.cc
@@ -9,7 +9,7 @@ void Http::listenAndServe() {
 
     for (;;) {
         std::pair<char*, std::pair<ssize_t, int>> conn = stream::NetworkStream::acceptTCP();
-        tcp::TCPConn tcpConn(conn.second.second, conn.first); // pass the socket file descriptor as parameter and also the first bytes readed
+        tcp::TCPConn tcpConn(conn.second.second, conn.first, conn.second.first); // pass the socket file descriptor as parameter and also the first bytes readed
          
     }
 }
--- src/server/http_server.hpp
+++ src/server/http_server.hpp
@@ -1,5 +1,6 @@
 
 #include <string>
+#include <vector>
 #include "../stream/network_stream.hpp"
 #include "../tcp/tcp.hpp"
 
@@ -16,7 +17,60 @@ class Http : public stream::NetworkStream {
         void listenAndServe();
     private:
         void handlePacket(tcp::TCPConn& conn) {
+            std::vector<char> mergedChunks;
+            std::vector<char> scanStack;
+
+            std::pair<char*, ssize_t> buffer = conn.readInitialBuffer();
+            char *data = buffer.first;
+            ssize_t totalBytes = buffer.second;
+            if (isHeaderReached(data, totalBytes)) {
+                // TODO-> how to respond?
+            }
+
+            do {
+                buffer = conn.readUntil();
+                data = buffer.first;
+                totalBytes = buffer.second;
+
+
+            } while (isHeaderReached(data, totalBytes));
             
+        };
+
+        bool isHeaderReached(char *data, ssize_t totalBytes) {
+            std::vector<char> scanStack;
+
+            for (ssize_t offset = 0; offset < totalBytes; offset++) {
+                scanStack.push_back(data[offset]);
+                if (scanStack.size() < 4) {
+                    continue;
+                }
+
+                char nextShouldBe = '\r';
+                for (auto it = scanStack.begin(); it != scanStack.end(); it++) {
+                    switch (*it) {
+                        case '\r':
+                            if (nextShouldBe == '\r') {
+                                nextShouldBe = '\n';
+                                break;
+                            } else {
+                                return false;
+                            }
+                        case '\n':
+                            if (nextShouldBe == '\n') {
+                                nextShouldBe = '\r';
+                                break;
+                            } else { 
+                                return false;
+                            }
+                        default:
+                            scanStack.clear();
+                            return false;
+                    }
+                    
+                }
+            }
+            return false;
         };
 
         std::string address;
