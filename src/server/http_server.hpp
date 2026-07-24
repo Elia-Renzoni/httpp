97c3c6b9c084a001f972b3a82336a24030860c94
Elia Renzoni <elia.renzoni03@gmail.com>
2026-07-24 16:52:47 +0200

    fix
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- src/server/http_server.hpp
+++ src/server/http_server.hpp
@@ -1,6 +1,7 @@
 
 #include <string>
 #include "../stream/network_stream.hpp"
+#include "../tcp/tcp.hpp"
 
 namespace server {
  
@@ -8,10 +9,15 @@ class Http : public stream::NetworkStream {
     public:
         Http(std::string& addr, int port): address(addr), port(port), maxHeaderBytes(16384), stream::NetworkStream(addr, port) {};
         Http(std::string addr, int port, int maxHeaderBytes): address(addr), port(port), maxHeaderBytes(maxHeaderBytes), stream::NetworkStream(addr, port){};
+        Http(std::string addr, int port, int maxHeaderBytes, int customRecvBufferSSize): address(addr), port(port), maxHeaderBytes(maxHeaderBytes), stream::NetworkStream(addr, port){};
+
         ~Http() = default;
 
         void listenAndServe();
     private:
+        void handlePacket(tcp::TCPConn& conn) {
+        };
+
         std::string address;
         int port;
         size_t  maxHeaderBytes; // 16kb as a default value
