23b3caf67c73b924ed1d0875e82e52dac6efb41d
Elia Renzoni <elia.renzoni03@gmail.com>
2026-07-26 10:30:25 +0200

    perf
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- src/server/http_server.hpp
+++ src/server/http_server.hpp
@@ -1,9 +1,11 @@
 
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <iostream>
 #include "../stream/network_stream.hpp"
 #include "../tcp/tcp.hpp"
+#include "../parsing/parser.hpp"
+#include "../parsing/scanner.hpp"
 
 namespace server {
  
@@ -17,24 +19,71 @@ class Http : public stream::NetworkStream {
 
         void listenAndServe();
     private:
+        std::string buildHTTPResponse(std::string statusCode, std::string reason, std::string body) {
+            std::string response =
+                "HTTP/1.1 " + statusCode + " " + reason + "\r\n"
+                "Content-Type: text/plain\r\n"
+                "Content-Length: " + std::to_string(body.size()) + "\r\n"
+                "Connection: close\r\n"
+                "\r\n" + body;
+            return response;
+        }
+
         void handleConnection(tcp::TCPConn& conn) {
             std::vector<char> mergedChunks;
+            // counter for all the bytes readed per buffer
+            ssize_t totalBytes;
 
             std::pair<char*, ssize_t> buffer = conn.readInitialBuffer();
             char *data = buffer.first;
-            ssize_t totalBytes = buffer.second;
+            totalBytes +=  buffer.second;
             if (isHeaderReached(data, totalBytes)) {
-                // TODO-> how to respond?
+                goto PARSE;
+            }
+
+            if (totalBytes < maxHeaderBytes) {
+                mergedChunks.insert(mergedChunks.end(), data, data + totalBytes);
+            } else {
+                std::string response = buildHTTPResponse("431", "Request Header Fields Too Large", "");
+                conn.write(response);
+                conn.closeConn();
             }
 
             do {
                 buffer = conn.readUntil();
                 data = buffer.first;
-                totalBytes = buffer.second;
+                totalBytes += buffer.second;
+                mergedChunks.insert(mergedChunks.end(), data, data + totalBytes);
+            } while (!isHeaderReached(data, totalBytes) && totalBytes <= maxHeaderBytes);
 
-
-            } while (isHeaderReached(data, totalBytes));
+            if (!isHeaderReached(mergedChunks.data(), totalBytes)) {
+                std::string response = buildHTTPResponse("400", "Bad Request", "");
+                conn.write(response);
+                conn.closeConn();
+            }
             
+            if (totalBytes > maxHeaderBytes) {
+                std::string response = buildHTTPResponse("431", "Request Header Fields Too Large", "");
+                conn.write(response);
+                conn.closeConn();
+            }
+
+PARSE:
+            parsing::TokensManager tm = parsing::TokensManager();
+            parsing::Scanner lexer = parsing::Scanner(tm, mergedChunks.data(), totalBytes);
+            parsing::Parser parser = parsing::Parser(lexer);
+
+            try {
+                parser.parseRequestLine();
+                parser.parseGenAndEntityHeader();
+            } catch (parsing::ParserException& exp) {
+                std::string response = buildHTTPResponse("400", "Bad Request", exp.what());
+                conn.write(response);
+                conn.closeConn();
+            }
+
+            // TODO-> handle endpoint rounting
+
         };
 
         bool isHeaderReached(char *data, ssize_t totalBytes) {
