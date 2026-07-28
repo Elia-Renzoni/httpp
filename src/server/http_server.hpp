11e28d418e5413da5c17984bc7edc0ef495c89b9
Elia Renzoni <elia.renzoni@gmail.com>
2026-07-28 17:25:46 +0200

    chore: add handler registry
    Signed-off-by: Elia Renzoni <elia.renzoni@gmail.com>

--- src/server/http_server.hpp
+++ src/server/http_server.hpp
@@ -1,14 +1,18 @@
 
 #include <string>
 #include <vector>
+#include <functional>
 #include <iostream>
 #include "../stream/network_stream.hpp"
 #include "../tcp/tcp.hpp"
 #include "../parsing/parser.hpp"
 #include "../parsing/scanner.hpp"
+#include "models.hpp"
 
 namespace server {
- 
+
+using HttpHandler = std::function<void(Request, Response&)>;
+
 class Http : public stream::NetworkStream {
     public:
         Http(std::string& addr, int port): address(addr), port(port), maxHeaderBytes(16384), stream::NetworkStream(addr, port) {};
@@ -18,6 +22,7 @@ class Http : public stream::NetworkStream {
         ~Http() = default;
 
         void listenAndServe();
+        void handleFunc(std::string endpoint, HttpHandler handler);
     private:
         std::string buildHTTPResponse(std::string statusCode, std::string reason, std::string body) {
             std::string response =
@@ -61,7 +66,7 @@ class Http : public stream::NetworkStream {
                 conn.write(response);
                 conn.closeConn();
             }
-            
+
             if (totalBytes > maxHeaderBytes) {
                 std::string response = buildHTTPResponse("431", "Request Header Fields Too Large", "");
                 conn.write(response);
@@ -100,6 +105,7 @@ PARSE:
         std::string address;
         int port;
         size_t  maxHeaderBytes; // 16kb as a default value
+        std::unordered_map<std::string, HttpHandler> routeMap;
 };
 
 }
---
+++ src/server/models.hpp
@@ -0,0 +1,15 @@
+
+
+
+namespace {
+
+struct Request {
+
+};
+
+struct Response {
+
+};
+
+
+}
