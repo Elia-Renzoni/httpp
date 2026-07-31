6410782bf8ed18fad6be7247367e23acaad43ad7
Elia Renzoni <elia.renzoni@gmail.com>
2026-07-31 16:39:47 +0200

    feat: add FVT test
    Signed-off-by: Elia Renzoni <elia.renzoni@gmail.com>

---
+++ proof/httpp_server.cc
@@ -0,0 +1,17 @@
+
+
+#include "../src/server/http_server.hpp"
+
+void handler(Request req, Response& res) {
+
+}
+
+int main() {
+    std::string addr = "127.0.0.1";
+    int listenPort = 8080;
+
+    server::Http instance = server::Http(addr, listenPort);
+    instance.handleFunc("/proof", handler);
+
+    instance.listenAndServe();
+}
